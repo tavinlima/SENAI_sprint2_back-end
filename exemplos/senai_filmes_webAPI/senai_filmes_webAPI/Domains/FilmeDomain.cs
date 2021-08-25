e00267523d3469ee6b4ce78685cdd3a20f7a4b5b
Gustavo Lima <glima.0913@gmail.com>
2021-08-25 16:12:30 -0300

    Criação do exemplo de filmes em API

---
+++ exemplos/senai_filmes_webAPI/senai_filmes_webAPI/Domains/FilmeDomain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_filmes_webAPI.Domains
+{
+    /// <summary>
+    /// Classe que representa a entidade (tabela) Filme
+    /// </summary>
+    public class FilmeDomain
+    {
+        public int idFilme { get; set; }
+        public int idGenero { get; set; }
+        public string tituloFilme { get; set; }
+        public GeneroDomain genero { get; set; }
+    }
+}
---
+++ exemplos/senai_filmes_webAPI/senai_filmes_webAPI/Domains/GeneroDomain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_filmes_webAPI.Domains
+{
+    /// <summary>
+    /// Classe que representa a entidade (tabela) Genero
+    /// </summary>
+    public class GeneroDomain
+    {
+        public int idGenero { get; set; }
+        public string nomeGenero { get; set; }
+    }
+}
